8a111ff9bb72cb0a6a28bd8490ed884ff53c9ea5
sathya177 <47978849+sathya177@users.noreply.github.com>
2019-06-18 09:44:45 +0000

    Update V1_1__INSERT_MASTER_DATA.sql

--- Data/V1_1__INSERT_MASTER_DATA.sql
+++ Data/V1_1__INSERT_MASTER_DATA.sql
@@ -1,6 +1,4 @@
-
-
-INSERT INTO OrderStatus VALUES ( 'ORDER PLACED')
-INSERT INTO OrderStatus VALUES( 'ORDER PROCESSING')
-INSERT INTO OrderStatus VALUES ( 'ORDER OUT FOR DELIVERY')
-INSERT INTO OrderStatus VALUES ( 'Completed Order')
+INSERT INTO OrderStatus(Id,[StatusDesc]) VALUES (1,'ORDER PLACED')
+INSERT INTO OrderStatus(Id,[StatusDesc]) VALUES(2,'ORDER PROCESSING')
+INSERT INTO OrderStatus(Id,[StatusDesc]) VALUES (3,'ORDER OUT FOR DELIVERY')
+INSERT INTO OrderStatus(Id,[StatusDesc]) VALUES (4,'Completed Order')
